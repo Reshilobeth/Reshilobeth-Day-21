5eeec269e6b1b5e10ffe6f637861a3635287639c
Reshilobeth <92940855+Reshilobeth@users.noreply.github.com>
2022-03-14 11:31:23 +0530

    Add files via upload

---
+++ 21/app.js
@@ -0,0 +1,39 @@
+let display = document.querySelector('.display')
+
+let countDown = 1
+
+
+
+setTimeout(()=>{
+    display.innerHTML = countDown++
+    setTimeout(()=>{
+        display.innerHTML = countDown++
+        setTimeout(()=>{
+            display.innerHTML = countDown++
+            setTimeout(()=>{
+                display.innerHTML = countDown++
+                setTimeout(()=>{
+                    display.innerHTML = countDown++
+                    setTimeout(()=>{
+                        display.innerHTML = countDown++
+                        setTimeout(()=>{
+                            display.innerHTML = countDown++
+                            setTimeout(()=>{
+                                display.innerHTML = countDown++
+                                setTimeout(()=>{
+                                    display.innerHTML = countDown++
+                                    setTimeout(()=>{
+                                        display.innerHTML = countDown++
+                                        setTimeout(()=>{
+                                            display.innerHTML ='Happy Independence Day!!'
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
